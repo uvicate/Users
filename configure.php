6a8e1c5f3d1613cd74af9887360765c2a30676ad
victorhqc <victorhqc@gmail.com>
2013-11-07 11:07:21 -0700

    DB configuration

---
+++ configure.php
@@ -0,0 +1,9 @@
+<?php
+$r = __DIR__.'/'; // root
+$vendor = $r.'vendor/';
+require_once $vendor.'autoload.php';
+
+$GLOBALS['user_pdo'] = new PDO("mysql:host=localhost;dbname=users", "root", "mnkDazul}mysql");
+$GLOBALS['user_fpdo'] = new FluentPDO($GLOBALS['user_pdo']);
+
+?>
